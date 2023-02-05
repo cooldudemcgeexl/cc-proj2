04ba575c4e3b83b7c60aad2e61b5d9fa0780d23c
Nick Lay <layns@mail.uc.edu>
2023-02-05 13:15:48 -0500

    add page layouts

--- proj2/app/app.py
+++ proj2/app/app.py
@@ -1,9 +1,19 @@
-from blueprints import root
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+from ..blueprints import auth, root
+
+db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
+
+    app.config['SECRET_KEY'] = 'key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
 
     app.register_blueprint(root)
+    app.register_blueprint(auth)
+
     return app
--- proj2/blueprints/root.py
+++ proj2/blueprints/root.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 root = Blueprint('root', __name__)
 
 @root.route('/')
-def test():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
 
+@root.route('/profile')
+def profile():
+    return render_template('profile.html')
